73a6580f8993024b98413525e117de0eca40d709
Mohit-Arya1211 <mohit.arya1211@gmail.com>
2020-04-19 16:11:24 +0530

    Serevr Connection Added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var faker = require('faker');
 const mysql = require('mysql');
+var express = require('express');
 
 
 
@@ -12,11 +13,32 @@ var connection = mysql.createConnection({
     port: 3306
         //multipleStatements: true
 });
+
 //Enter the query: 
-connection.connect();
+//connection.connect();
 
 //running Query:
 
+// Query to get the count :
+//Routes for App by Express.Js
+
+var app = express();
+app.get("/", function(req, res) {
+
+    var q2 = "select count(*) as count from users";
+    connection.query(q2, function(err, result) {
+        if (err) {
+            return console.log(err);
+        }
+        var count = result[0].count;
+        res.send("We have " + count + " users in our database.");
+    });
+
+});
+
+
+
+//##########################################################################################
 //For entering the 500 dummy Users Loop:
 
 // for (var i = 0; i < 500; i++) {
@@ -29,16 +51,22 @@ connection.connect();
 //         console.log(result);
 //     })
 // }
+//############################################################################################
+//Query for running any operaion:
+// var query = connection.query(
+//     'select email from users;',
+//     function(err, result, fields) {
+//         if (err) {
+//             return console.log(err);
+//         } else {
+//             console.log("result : ", result);
+//         }
+//     });
 
-var query = connection.query(
-    'select email;',
-    function(err, result, fields) {
-        if (err) {
-            return console.log(err);
-        } else {
-            console.log("result : ", result);
-        }
-    });
 
+// Server Added :
+app.listen(1211, function() {
+    console.log("App running on port : 1211");
+});
 
-connection.end();
+//connection.end();
