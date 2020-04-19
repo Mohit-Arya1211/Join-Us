a056ccaafee9ac96e143b2c66e35541d8ceff8ae
Mohit-Arya1211 <mohit.arya1211@gmail.com>
2020-04-19 17:07:48 +0530

    final Commit

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 var express = require('express');
 var mysql = require('mysql');
-var bodyParser = require("body-parser");
+var bodyParser  = require("body-parser");
 var app = express();
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123445',
-    database: 'join_us'
+  host     : 'localhost',
+  user     : 'learnwithcolt',
+  database : 'join_us'
 });
 
-app.get("/", function(req, res) {
+app.get("/", function(req, res){
     // Find count of users in DB
     var q = "SELECT COUNT(*) AS count FROM users";
-    connection.query(q, function(err, results) {
-        if (err) throw err;
-        var count = results[0].count;
-        res.render("home", { count: count });
+    connection.query(q, function(err, results){
+        if(err) throw err;
+        var count = results[0].count; 
+        res.render("home", {count: count});
     });
 });
 
-app.post("/register", function(req, res) {
+app.post("/register", function(req, res){
     var person = {
         email: req.body.email
     };
@@ -34,6 +33,6 @@ app.post("/register", function(req, res) {
     });
 });
 
-app.listen(8080, function() {
+app.listen(8080, function(){
     console.log("Server running on 8080!");
 });
